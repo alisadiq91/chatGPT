4d1c90de8bdc0fba1507b9d7eb4502b5e723739e
alisadiq91 <ali.sadiq91@gmail.com>
2023-01-22 22:51:31 +0000

    tic tac toe done

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-// Get all the links with the class 'scroll'
-var links = document.querySelectorAll('.scroll');
+var currentPlayer = "X";
+var gameWon = false;
+var cells = document.querySelectorAll("td");
+var winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-// Loop through the links and add an event listener to each one
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener('click', function(e) {
-    e.preventDefault();
-    var sectionId = this.getAttribute("href");
-    document.querySelector(sectionId).scrollIntoView({
-      behavior: 'smooth'
-    });
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", function() {
+    if (!gameWon) {
+      if (this.textContent === "") {
+        this.textContent = currentPlayer;
+        checkForWin();
+        switchPlayer();
+      }
+    }
   });
 }
 
-var elements = document.querySelectorAll('.hover-effect');
-
-for (var i = 0; i < elements.length; i++) {
-  elements[i].addEventListener('mouseover', function() {
-    this.style.backgroundColor = '#f2f2f2';
-  });
-  elements[i].addEventListener('mouseout', function() {
-    this.style.backgroundColor = '#fff';
-  });
+function checkForWin() {
+  for (var i = 0; i < winCombinations.length; i++) {
+    if (cells[winCombinations[i][0]].textContent === currentPlayer &&
+        cells[winCombinations[i][1]].textContent === currentPlayer &&
+        cells[winCombinations[i][2]].textContent === currentPlayer) {
+      gameWon = true;
+      cells[winCombinations[i][0]].classList.add("winner");
+      cells[winCombinations[i][1]].classList.add("winner");
+      cells[winCombinations[i][2]].classList.add("winner");
+      alert(currentPlayer + " wins!");
+    }
+  }
 }
+
+function switchPlayer() {
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+}
+
+var resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+document.body.appendChild(resetButton);
+resetButton.addEventListener("click", function() {
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].textContent = "";
+    cells[i].classList.remove("winner");
+  }
+  gameWon = false;
+});
